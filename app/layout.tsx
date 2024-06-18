732bb722af67f5f087eae546055dcfa68a96b47c
Takashi Himeoka <himeoka@stoopa.org>
2024-06-18 19:11:27 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,8 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
   return (
     <html lang="en" className={GeistSans.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
-        <Navbar />
         <Suspense>
+          <Navbar />
           <main>{children}</main>
         </Suspense>
       </body>
